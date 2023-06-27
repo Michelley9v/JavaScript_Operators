8ae4f8a171538497bb426f977250fe0038467e61
Michelle Hoffmann <michelley9v@yahoo.com>
2023-06-26 19:47:45 -0500

    Objective Qu

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 let a = 20;
 let b = 4;
+a + b
   20 + 4 ; // 24
+  a - b
   20 - 4 ; // 16
+  a * b
   20 * 4 ; // 80
+  a / b
   20 / 4 ; // 5
 
 console.log ("add");
@@ -22,6 +26,18 @@ let str2 = "eleven";
 let isPresent = true;
 let firstName = "Frodo";
 let lastName = "Baggins";
+num + str 
+'1111'
+num + str2
+"11eleven" 
+num + isPresent
+12
+firstName + num
+"Frodoo11"
+isPresent + str
+"true11"
+firstName + lastName
+"FrodoBaggins"
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
@@ -30,3 +46,21 @@ let val = 5;
 let str3 = "5";
 let str4 = "five";
 let isPresent2 = false;
+val == str3
+true
+val === str3
+false
+!isPresent2
+true
+“eleven” == str4 && val >= str3
+invalid character
+!isPresent2 || isPresent2
+error can't find variable in isPresent2
+0 == false
+true
+0 === false
+false
+0 != false
+false
+0 !== false
+true
